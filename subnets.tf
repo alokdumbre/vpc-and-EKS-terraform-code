87359896a35a84b6f7d4d60d504379cf6982048e
alokdumbre <56631296+alokdumbre@users.noreply.github.com>
2021-10-27 06:17:27 +0000

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -7,14 +7,14 @@ resource "aws_subnet" "public_1" {
   cidr_block = "10.0.1.0/24"
 
   # The AZ for the subnet.
-  availability_zone = "ap-southeast-2a"
+  availability_zone = "ap-south-1a"
 
   # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 
   # A map of tags to assign to the resource.
   tags = {
-    Name                        = "public-ap-southeast-2a"
+    Name                        = "public-ap-south-1a"
     "kubernetes.io/cluster/eks" = "shared"
     "kubernetes.io/role/elb"    = 1
   }
@@ -27,14 +27,14 @@ resource "aws_subnet" "public_2" {
   cidr_block = "10.0.2.0/24"
 
   # The AZ for the subnet.
-  availability_zone = "ap-southeast-2b"
+  availability_zone = "ap-south-1b"
 
   # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 
   # A map of tags to assign to the resource.
   tags = {
-    Name                        = "public-ap-southeast-2b"
+    Name                        = "public-ap-south-1b"
     "kubernetes.io/cluster/eks" = "shared"
     "kubernetes.io/role/elb"    = 1
   }
@@ -48,11 +48,11 @@ resource "aws_subnet" "private_1" {
   cidr_block = "10.0.3.0/24"
 
   # The AZ for the subnet.
-  availability_zone = "ap-southeast-2a"
+  availability_zone = "ap-south-1a"
 
   # A map of tags to assign to the resource.
   tags = {
-    Name                              = "private-ap-southeast-2a"
+    Name                              = "private-ap-south-1a"
     "kubernetes.io/cluster/eks"       = "shared"
     "kubernetes.io/role/internal-elb" = 1
   }
@@ -66,11 +66,11 @@ resource "aws_subnet" "private_2" {
   cidr_block = "10.0.4.0/24"
 
   # The AZ for the subnet.
-  availability_zone = "ap-southeast-2b"
+  availability_zone = "ap-south-1b"
 
   # A map of tags to assign to the resource.
   tags = {
-    Name                              = "private-ap-southeast-2b"
+    Name                              = "private-ap-south-1b"
     "kubernetes.io/cluster/eks"       = "shared"
     "kubernetes.io/role/internal-elb" = 1
   }
